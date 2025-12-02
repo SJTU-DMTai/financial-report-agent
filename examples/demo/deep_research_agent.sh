392fcabb354d1b336af0ceb45e464bf259815711
YYJ <630794886@qq.com>
2025-12-02 13:51:41 +0800

    update

--- examples/demo/deep_research_agent.sh
+++
@@ -1,5 +0,0 @@
-export DASHSCOPE_API_KEY="your_dashscope_api_key_here"
-export TAVILY_API_KEY="your_tavily_api_key_here"
-export AGENT_OPERATION_DIR="your_own_direction_here"
-
-npx -y tavily-mcp@latest
